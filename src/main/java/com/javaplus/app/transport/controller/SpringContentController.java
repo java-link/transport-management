7416123e3c1d03b1cd2a83faaed036d70729b610
survesac <survesac@WL304179.ap.tieto.com>
2014-12-26 17:01:21 +0530

    login changes

--- src/main/java/com/javaplus/app/transport/controller/SpringContentController.java
+++ src/main/java/com/javaplus/app/transport/controller/SpringContentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SpringContentController {
@@ -22,5 +23,27 @@ public class SpringContentController {
 		userDetails.setEmailId("jainshi@gmail.com");
 		return userDetails;
 	}
+	
+	@RequestMapping(value = "/welcomePage", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+ 
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("secured/welcomePage");
+ 
+		return model;
+ 
+	}
+	
+    @RequestMapping(value = "/logoutPage", method = RequestMethod.GET)
+    public String logoutPage() {
+        return "logoutPage";
+    }
+	
+    @RequestMapping(value = "/loginPage", method = RequestMethod.GET)
+    public String loginPage() {
+        return "loginPage";
+    }
 	
 }
